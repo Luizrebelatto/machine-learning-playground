477f6799dd7916153909296cfc17c126689cd045
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-03-07 21:43:27 -0300

    function to load model and labels

--- ai-game-training/machine-learning/worker.js
+++ ai-game-training/machine-learning/worker.js
@@ -3,6 +3,15 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 
+let _labels = []
+let _model = null
+async function loadModelAndLabels() {
+    await tf.ready()
+    _labels = await (await fetch(LABELS_PATH)).json()
+    _model = await tf.loadGraphModel(MODEL_PATH)
+}
+loadModelAndLabels()
+
 
 self.onmessage = async ({ data }) => {
     if (data.type !== 'predict') return
